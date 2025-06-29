574a96951ba4ce518540090df2f771127255e0b0
Archana-Prajesh <archanaprajesh13@gmail.com>
2025-06-29 17:44:08 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import Banner from './Banner';
 function App() {
   return (
         <div>
-      <Header></Header>
+      <h1>It is working!</h1>
+      <p>If you're seeing this, your app is deployed successfully.</p>
     </div>
   );
 }
