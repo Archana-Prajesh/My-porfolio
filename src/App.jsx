e8f274bfb6f2bdb13fa7015b40cba75b8a48d6f2
Archana-Prajesh <archanaprajesh13@gmail.com>
2025-06-29 21:17:58 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import Banner from './Banner';
 function App() {
   return (
         <div>
-      <h1>It is working!</h1>
-      <p>If you're seeing this, your app is deployed successfully.</p>
+      <h1>Welcome In React </h1>
+      <p>React is amazing Library</p>
     </div>
   );
 }
