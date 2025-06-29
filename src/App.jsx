22ca801e0c093c0b6523b9f81c37f592b0af1059
Archana-Prajesh <archanaprajesh13@gmail.com>
2025-06-29 21:36:06 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 function App() {
   return (
         <div>
-        <h1 className="text-4xl font-bold text-blue-600">Welcome to React</h1>
-        <p className="mt-4 text-gray-700">React is an amazing library</p>
+        <h1>Welcome to React</h1>
+        <p>React is an amazing library</p>
       </div>
   );
 }
