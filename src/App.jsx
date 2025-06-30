d18ccdcecc57b2d604ddb53a0538351fa456a6a9
Archana-Prajesh <archanaprajesh13@gmail.com>
2025-06-30 15:24:13 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import Header from './Header';
 function App() {
   return (
-      <div>
+      <>
         <h1>Welcome to React</h1>
         <p>React is an amazing library</p>
         <Header />
-      </div>
+      </>
   );
 }
 
