4a5052a30e6d60e9cc624ded2395a80eea5421ff
Archana-Prajesh <archanaprajesh13@gmail.com>
2025-06-30 14:47:53 +0530

    Update Header.jsx

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 export default function Header() {
   return (
     <header className="header">
-      <div className="logo"><img className="logo-image" src="/Image.png" alt="My Image" /></div>
+      <div className="logo">
+        <img className="logo-image" src="/Image.png" alt="My Image" />
+      </div>
       <nav className="nav">
         <a href="#home">Home</a>
         <a href="#about">About</a>
@@ -12,7 +14,12 @@ export default function Header() {
         <a href="#projects">Projects</a>
         <a href="#contact">Contact</a>
       </nav>
-      <div><span className="contact-number"><img className="phone-image" src="/download.svg" alt="phone" /> +1 971 234 1508</span></div>
+      <div>
+        <span className="contact-number">
+          <img className="phone-image" src="/download.svg" alt="phone" /> +1 971 234 1508
+        </span>
+      </div>
     </header>
   );
 }
+
